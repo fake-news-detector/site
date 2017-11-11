013bcb343cc0805288513b88ea931f59600d2c37
Rogerio Chaves <rogeriocfj@gmail.com>
2017-11-11 09:44:51 -0300

    Make build more simple by using html pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,6 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const fs = require("fs");
-
-// Generates htmls for each page generates by elm-static-html
-const pages = fs.readdirSync("./build/static/pages");
-const pagesPlugins = pages.map(
-  page =>
-    new HtmlWebpackPlugin({
-      template: "src/index.ejs",
-      filename: page,
-      page: page.replace(".html", ""),
-      alwaysWriteToDisk: true
-    })
-);
 
 const config = {
   entry: "./src/index.js",
@@ -36,19 +23,24 @@ const config = {
       }
     ]
   },
-  devServer: {
-    host: "0.0.0.0",
-    disableHostCheck: true
-  },
-  plugins: pagesPlugins.concat([
+  plugins: [
     new webpack.EnvironmentPlugin(["DEBUG"]),
+    new HtmlWebpackPlugin({
+      template: "src/pages/index.ejs",
+      filename: "index.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/pages/more.ejs",
+      filename: "more.html",
+      inject: false
+    }),
     new CopyWebpackPlugin([
       {
         from: "src/static",
         to: "static"
       }
     ])
-  ])
+  ]
 };
 
 module.exports = config;
